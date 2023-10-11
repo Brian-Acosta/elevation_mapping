c888f8cc86c4e3a614f2391b1f62ce8142c2838c
Brian Acosta <bjacosta@seas.upenn.edu>
2023-10-11 15:42:30 -0400

    add includes to test

--- tools/test_bazel_deps/test_includes.cc
+++ tools/test_bazel_deps/test_includes.cc
@@ -1,9 +1,14 @@
 #include <iostream>
-#include
+#include "drake/math/rigid_transform.h"
+#include "grid_map_core/grid_map_core.h"
+#include "pcl/point_cloud.h"
+#include "pcl/point_types.h"
 
 namespace elevation_mapping {
 
 int do_main() {
+  drake::math::RigidTransformd transform = drake::math::RigidTransformd::Identity();
+  pcl::PointCloud<pcl::PointXYZ> point_cloud;
   std::cout << "Success" << std::endl;
   return 0;
 }
